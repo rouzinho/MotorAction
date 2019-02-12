a30d2a4e3310e2faa5f940ff23b798f27eca9d73
guy <q.houbre@gmail.com>
2019-02-12 15:09:52 +0200

    random actions work, with a locker to the current one

--- MotorAction/MotorAction.cpp
+++ MotorAction/MotorAction.cpp
@@ -4,7 +4,8 @@
 #include <cedar/processing/ExternalData.h> // getInputSlot() returns ExternalData
 #include <cedar/auxiliaries/MatData.h> // this is the class MatData, used internally in this step
 #include "cedar/auxiliaries/math/functions.h"
-#include <random>
+#include<cstdlib>
+#include<ctime>
 
 // SYSTEM INCLUDES
 
@@ -22,7 +23,7 @@ mSigma(new cedar::aux::DoubleParameter(this,"Sigma",3.0))
 this->declareOutput("output", mOutput);
 this->declareInput("ready",true);
 this->declareInput("explore",true);
-
+srand(static_cast<unsigned int>(clock()));
 mGaussMatrixSizes.push_back(100);
 mGaussMatrixSigmas.push_back(3.0);
 mGaussMatrixCenters.push_back(25.0);
@@ -47,15 +48,14 @@ void MotorAction::compute(const cedar::proc::Arguments&)
   auto ready_peak = boost::dynamic_pointer_cast<cedar::aux::ConstMatData>(this->getInput("ready"));
   exploration = cedar::aux::math::getMatrixEntry<double>(exp->getData(), 0, 0);
   ready = cedar::aux::math::getMatrixEntry<double>(ready_peak->getData(), 0, 0);
+  std::cout << "ready : " << ready << '\n';
 
-  if (exploration > 0.9)
+  if (exploration < 0.8)
   {
      if(ready > 0.8 && lock_action == 1)
      {
         mGaussMatrixCenters.clear();
-        std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
-        std::default_random_engine re;
-        next_action = unif(re);
+        next_action = double(rand()) / (double(RAND_MAX) + 1.0)*100;
         std::cout << "next action " << next_action <<'\n';
         mGaussMatrixCenters.push_back(next_action);
         dat = 2.0;
@@ -63,6 +63,7 @@ void MotorAction::compute(const cedar::proc::Arguments&)
      }
      if(ready < 0.8)
      {
+       std::cout << "not ready " << '\n';
        dat = 0;
        lock_action = 1;
      }
@@ -78,7 +79,6 @@ void MotorAction::reCompute()
    mGaussMatrixSizes.push_back(static_cast<int>(this->mSize->getValue()));
    mGaussMatrixSigmas.push_back(static_cast<double>(this->mSigma->getValue()));
    dat = static_cast<int>(this->mAmplitude->getValue());
-
 }
 
 
